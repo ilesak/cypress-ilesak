64d0cd33e6077be95465c261648ff76d3086353e
Irina Lesak <irinalesak@gmail.com>
2019-07-23 16:27:03 +0300

    clear extra code

--- cypress/page-objects/currencyConvertPage.js
+++ cypress/page-objects/currencyConvertPage.js
@@ -1,26 +1,3 @@
-/*class AccessoriesPage {
-    open(){
-        cy.visit(`${Cypress.env('googleURL')}/collection/accessories_wall?hl=en-US`);
-    }
-
-    get searchIcon(){
-        return cy.get('.header-search-icon');
-    }
-
-    get searchInput(){
-        return cy.get('input[aria-label="Search Google Store"]');
-    }
-
-    performSearch(productToSearch){
-        this.searchIcon.click();
-        this.searchInput.type(`${productToSearch}{enter}`);
-    }
-}
-
-export default new AccessoriesPage()*/
-
-import convertResultsPage from './convertResultsPage'
-
  class currencyConvertPage{
      open(){
          cy.visit('https://www.xe.com/currencyconverter/');
